4c5802542154a1c48a993f81f6b72f1c7953a1d5
zzr <zhangzhuoranty01@163.com>
2022-08-26 19:45:38 +0800

    111

--- 413.等差数列划分.py
+++ 413.等差数列划分.py
@@ -11,22 +11,20 @@ from operator import le
 
 class Solution:
     def numberOfArithmeticSlices(self, nums):
-        if(len(nums)<3):
+        if (len(nums) < 3):
             return 0
         n = len(nums)
         l = r = ans = 0
-        
-        while l < n-2 :
+
+        while l < n-2:
             d = nums[l+1] - nums[l]
             while r < n-1 and nums[r+1]-nums[r] == d:
                 r += 1
-            ans += (r-l) *(r-l-1)//2
+            ans += (r-l) * (r-l-1)//2
             l = r
         return ans
-            
-                
-        
-                
+
+
 # @lc code=end
 if __name__ == "__main__":
     sol = Solution()
